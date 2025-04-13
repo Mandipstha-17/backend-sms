73e8ae2b8b3cd28b4d4f78e1f602b1fe885c52a8
Mandip Shrestha <Mandeeparha123@gmail.com>
2025-04-13 09:05:47 +0545

    see

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 // Middlewares
 app.use(cors({
   origin: [
-    process.env.FRONTEND_URL,
+    "http://localhost:5173",
     "https://student-frontend-kappa.vercel.app"
   ],
   credentials: true
